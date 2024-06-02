0d819250bdf019204bb1b88fab8140878c46ef5a
paull <paull@NOSTRO>
2024-06-02 14:43:54 -0400

    Adicionar arquivos de projeto.

---
+++ RangoAgil.API/Entities/Ingrediente.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace RangoAgil.API.Entities;
+
+public class Ingrediente
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public required string Nome { get; set; }
+
+    public ICollection<Rango> Rangos { get; set; } = []; // [] semelhante a new List<Rango>()
+    public Ingrediente()
+    {
+            
+    }
+    [SetsRequiredMembers]
+    public Ingrediente(int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+    }
+}
+
---
+++ RangoAgil.API/Entities/Rango.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace RangoAgil.API.Entities;
+
+public class Rango
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public required string Nome { get; set; }
+
+    public ICollection<Ingrediente> Ingredientes { get; set; } = []; // [] semelhante a new List<Ingrediente>()
+
+    public Rango()
+    {
+            
+    }
+    [SetsRequiredMembers]
+    public Rango(int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+    }
+}
+
