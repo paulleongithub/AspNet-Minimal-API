1ccf3850dcf5500f670d5e781d4bfece659ede5d
Paul Leon Mourad <paulleonmourad@hotmail.com>
2024-10-19 12:56:32 -0400

    Seção 07 - Segurança e Documentação

--- RangoAgil.API/Program.cs
+++ RangoAgil.API/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using RangoAgil.API.DbContexts;
 using RangoAgil.API.EndpointHandlers;
 using RangoAgil.API.Extensions;
@@ -10,9 +12,52 @@ builder.Services.AddDbContext<RangoDbContext>(
     o => o.UseSqlite(builder.Configuration["ConnectionStrings:RangoDbConStr"])
 );
 
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<RangoDbContext>();
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddProblemDetails();
+
+builder.Services.AddAuthentication().AddJwtBearer();
+builder.Services.AddAuthorization();
+
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("RequireAdminFromBrazil", policy =>
+       policy
+          .RequireRole("admin")
+          .RequireClaim("country", "Brazil"));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("TokenAuthRango",
+        new()
+        {
+            Name = "Authorization",
+            Description = "Token baseado em Autenticação e Autorização",
+            Type =SecuritySchemeType.Http,
+            Scheme = "Bearer",
+            In = ParameterLocation.Header
+        }
+    );
+    options.AddSecurityRequirement(new()
+    {
+        {
+            new()
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "TokenAuthRango"
+                }
+            },
+            new List<string>()
+        }
+    }
+    );
+});
+
 
 var app = builder.Build();
 
@@ -32,6 +77,17 @@ if (!app.Environment.IsDevelopment())
     //        });
     //});
 
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 
